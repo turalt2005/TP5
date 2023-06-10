da0709523c43c6453136c6774ac7403368c5df09
turalt2005 <112487805+turalt2005@users.noreply.github.com>
2023-06-10 01:47:44 -0400

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 class GameState(Enum):
     NOT_STARTED = 0
-    ROUND_DONE = 1
-    GAME_OVER = 2
-    ROUND_ACTIVE = 3
+    GAME_OVER = 1
+    ROUND_ACTIVE = 2
+    ROUND_DONE = 3

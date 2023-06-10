9cf70bea8159130672769a5340ea13589f6835d8
turalt2005 <112487805+turalt2005@users.noreply.github.com>
2023-06-10 01:48:52 -0400

    Update attack_animation.py

--- attack_animation.py
+++ attack_animation.py
@@ -1,52 +1,52 @@
 from enum import Enum
+
 import arcade
 
 
-
 class AttackType(Enum):
-    ROCK = 0,
-    PAPER = 1,
-    SCISSORS = 2
+    ROCHE = 0
+    PAPIER = 1
+    CISEAUX = 2
 
 class AttackAnimation(arcade.Sprite):
-   ATTACK_SCALE = 0.50
-   ANIMATION_SPEED = 5.0
+    Attack_Scale = 0.5
+    Animation_Speed = 5
 
-   def __init__(self, attack_type):
-       super().__init__()
-       self.animation_update_time = 1.0 / AttackAnimation.ANIMATION_SPEED
-       self.time_since_last_swap = 0.0
+    def __init__(self, attack_type):
+        super().__init__()
 
-       self.attack_type = attack_type
-       if self.attack_type == AttackType.ROCK:
-           self.textures = [
-               arcade.load_texture("assets/srock.png"),
-               arcade.load_texture("assets/srock-attack.png"),
-           ]
-       elif self.attack_type == AttackType.PAPER:
-           self.textures = [
-               arcade.load_texture("assets/spaper.png"),
-               arcade.load_texture("assets/spaper-attack.png"),
-           ]
-       else:
-           self.textures = [
-               arcade.load_texture("assets/scissors.png"),
-               arcade.load_texture("assets/scissors-close.png"),
-           ]
+        self.attack_type = attack_type
+        self.animation_update_time = 1/AttackAnimation.Animation_Speed
+        self.time_since_last_swap = 0
 
-       self.scale = self.ATTACK_SCALE
-       self.current_texture = 0
-       self.set_texture(self.current_texture)
+        if self.attack_type == AttackType.ROCHE:
+            self.textures = [
+                arcade.load_texture("Assets/srock.png"),
+                arcade.load_texture("Assets/srock-attack.png"),
+            ]
+        elif self.attack_type == AttackType.PAPIER:
+            self.textures = [
+                arcade.load_texture("Assets/spaper.png"),
+                arcade.load_texture("Assets/spaper-attack.png"),
+            ]
+        else:
+            self.textures = [
+                arcade.load_texture("Assets/scissors.png"),
+                arcade.load_texture("Assets/scissors-close.png"),
+            ]
 
-       def on_update(self, delta_time: float = 1 / 60):
-           # Update the animation.
-           self.time_since_last_swap += delta_time
-           if self.time_since_last_swap > self.animation_update_time:
-               self.current_texture += 1
-               if self.current_texture < len(self.textures):
-                   self.set_texture(self.current_texture)
-               else:
-                   self.current_texture = 0
-                   self.set_texture(self.current_texture)
-               self.time_since_last_swap = 0.0
+        self.scale = self.Attack_Scale
+        self.current_texture = 0
+        self.set_texture(self.current_texture)
 
+    def on_update(self, delta_time: float = 1 / 60):
+        self.time_since_last_swap += delta_time
+
+        if self.time_since_last_swap > self.animation_update_time:
+            self.current_texture += 1
+            if self.current_texture < len(self.textures):
+                self.set_texture(self.current_texture)
+            else:
+                self.current_texture = 0
+                self.set_texture(self.current_texture)
+            self.time_since_last_swap = 0.0
